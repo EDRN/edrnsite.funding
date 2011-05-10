a08eced9d2e3d0adefa37786a808b67fc20f41da
Sean Kelly <sean.c.kelly@me.com>
2011-05-10 00:49:32 +0000

    Upgrade profile to v.4; add an upgrade module; define an upgrade path from 0 to 4.

---
+++ edrnsite/funding/upgrades.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# Copyright 2011 California Institute of Technology. ALL RIGHTS
+# RESERVED. U.S. Government Sponsorship acknowledged.
+
+def nullUpgradeStep(setupTool):
+    '''A null step for when a profile upgrade requires no custom activity.'''
