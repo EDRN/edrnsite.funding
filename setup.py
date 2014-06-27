7a11698b4924e056e913281b1b83caa816537973
Sean Kelly <kelly@seankelly.biz>
2014-06-26 20:43:31 -0500

    Release 1.0.5

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ import os.path
 _name        = 'edrnsite.funding'
 _version     = '1.0.5'
 _description = 'EDRN Funding Opportunities'
-_url         = 'http://cancer.jpl.nasa.gov/products/edrnsite-funding'
-_downloadURL = 'http://oodt.jpl.nasa.gov/dist/edrnsite'
 _author      = 'Sean Kelly'
 _authorEmail = 'sean.kelly@jpl.nasa.gov'
 _license     = 'ALv2'
@@ -54,7 +52,7 @@ def _read(*rnames):
 
 _header = '*' * len(_name) + '\n' + _name + '\n' + '*' * len(_name)
 _longDescription = _header + '\n\n' + _read('README.rst') + '\n\n' + _read('docs', 'INSTALL.txt') + '\n\n' \
-    + _read('docs', 'HISTORY.txt') + '\n\n' + _read('docs', 'LICENSE.txt')
+    + _read('docs', 'HISTORY.txt') + '\n'
 open('doc.txt', 'w').write(_longDescription)
 
 setup(
@@ -62,7 +60,6 @@ setup(
     author_email=_authorEmail,
     classifiers=_classifiers,
     description=_description,
-    download_url=_downloadURL,
     entry_points=_entryPoints,
     extras_require=_extras,
     include_package_data=True,
@@ -73,7 +70,7 @@ setup(
     name=_name,
     namespace_packages=_namespaces,
     packages=find_packages(exclude=['ez_setup']),
-    url=_url,
+    url='https://github.com/EDRN/' + _name,
     version=_version,
     zip_safe=_zipSafe,
 )
